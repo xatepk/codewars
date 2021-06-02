8a226d85e7ad62c86392fff9030938f6588bf796
Khatya Khakhutaishvili <kkhatya@gmail.com>
2021-06-02 21:41:46 +0300

    solutions

---
+++ 6kyu/stockQuotes.js
@@ -0,0 +1,31 @@
+// Description:
+//  Return the most profit from stock quotes.
+
+//  Stock quotes are stored in an array in order of date. The stock profit is the difference in prices in buying and selling stock. Each day, you can either buy one unit of stock, sell any number of stock units you have already bought, or do nothing. Therefore, the most profit is the maximum difference of all pairs in a sequence of stock prices.
+
+// @param {array} quotes
+// @return {number} max profit
+// Example:
+
+//  [ 1, 2, 3, 4, 5, 6 ]        => 15  (buy at 1,2,3,4,5 and then sell all at 6)
+//  [ 6, 5, 4, 3, 2, 1 ]        => 0   (nothing to buy for profit)
+//  [ 1, 6, 5, 10, 8, 7 ]       => 18  (buy at 1,6,5 and sell all at 10)
+//  [ 1, 2, 10, 3, 2, 7, 3, 2 ] => 26  (buy at 1,2 and sell them at 10. Then buy at 3,2 and sell them at 7)
+
+function getMostProfitFromStockQuotes(quotes) {
+    let count = 0;
+    let arr = quotes;
+    
+    while(arr.length > 0) {
+      let maxIndex = arr.indexOf( Math.max.apply(null, arr));
+       let ind = 0;
+      for (ind; ind<maxIndex; ind++) {
+          if (arr[maxIndex]-arr[ind] > 0) {
+            count+=(arr[maxIndex]-arr[ind]); 
+          }
+      }
+      arr.splice(0, maxIndex+1);
+    }
+    
+    return count;
+  }
