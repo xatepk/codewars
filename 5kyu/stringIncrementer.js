5af1232fdc08cf5cf9533a883dcd1b401435e0ab
xatepk <kkhatya@gmail.com>
2022-09-29 16:19:05 +0400

    solution

---
+++ 5kyu/stringIncrementer.js
@@ -0,0 +1,19 @@
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+function incrementString (strng) {
+    return strng.replace(/([0-8]|\d?9+)?$/, (e) => e ? + e + 1 : 1)
+}
